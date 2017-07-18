badc77104ab4de5809ecef009e8ea5eaa109e1f5
kaliRegenold <kali.regenold@mines.sdsmt.edu>
2017-07-18 00:29:29 -0500

    Adding day 2 material

---
+++ 2_Day/drive_straight_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import geekbot
+from time import sleep as wait
+
+geek = geekbot.Rboot("/dev/ttyUSB0", 57600)
+
+# Here, use the drive_forward command and change the 'adjust' parameter so your robot drive straight
+
